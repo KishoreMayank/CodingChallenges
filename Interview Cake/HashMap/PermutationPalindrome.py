e62f9d94512f4487b355774dbf62043dc8c5b5f8
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-23 14:24:07 -0500

    Bracket Validator

---
+++ Interview Cake/HashMap/PermutationPalindrome.py
@@ -0,0 +1,3 @@
+'''
+Permutation Palindrome:
+    
---
+++ Interview Cake/Stacks and Queues/BracketValidator.py
@@ -0,0 +1,26 @@
+'''
+Bracket Validator:
+    Write a function to check whether the opener's and closers are properly nested
+    Input: '{ [ ] ( ) }'
+    Output: True
+'''
+
+def is_valid(string):
+    open_close = {'(' : ')','{' : '}','[' : ']'}
+    openers = set(open_close.keys())
+    closers = set(open_close.values())
+
+    stack = []
+    for char in string:
+        if char in openers:
+            stack.append(char)
+        elif char in closers:
+            if not stack: # if the stack it empty it's false
+                return False
+            else:
+                if not open_close[stack.pop()] == char: # if the closer does not corren
+                    return False
+
+    return not stack
+
+

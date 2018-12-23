d2b6a1fb5c733ad91960257307d88f88cf4a6b68
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-23 01:00:27 -0500

    quick update

--- Interview Cake/Arrays and Strings/MergeLists.py
+++ Interview Cake/Arrays and Strings/MergeLists.py
@@ -6,7 +6,7 @@ Merge Lists:
     Output: [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
 '''
  
- def merge_lists(list1, list2):
+def merge_lists(list1, list2):
     merged_size = len(list1) + len(list2)
     merged = [None] * merged_size # initialize a list of len1 + len2
 

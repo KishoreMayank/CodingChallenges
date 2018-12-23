853e7dcacc0f6a2eed4cd5d54a3a5ad57f694f73
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-23 14:56:17 -0500

    Update PermutationPalindrome.py

--- Interview Cake/HashMap/PermutationPalindrome.py
+++ Interview Cake/HashMap/PermutationPalindrome.py
@@ -1,3 +1,22 @@
 '''
 Permutation Palindrome:
-    
+    Write an efficient function that checks whether any permutation of an input string is a palindrome.
+    Ex: "ivicc" should return True
+        "civil" should return False
+'''
+
+def permutation_palindrome(string):
+    unpaired = set() # Track characters we've seen an odd number of times
+
+    for char in string:
+        if char in unpaired:
+            unpaired.remove(char) # remove if even number of times
+        else:
+            unpaired.add(char) # add if odd number of times
+
+    return len(unpaired) <= 1 # more than one odd means no permutation
+
+
+
+
+

6ef93e0fb9fa4f7b10a3ff4ab09626f9cb4c65f6
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-23 20:36:33 -0500

    Create MaxProfit.py

---
+++ Interview Cake/Greedy/MaxProfit.py
@@ -0,0 +1,21 @@
+'''
+Max Profit:
+    Write an efficient function that takes stock_prices and returns the best profit 
+    I could have made from one purchase and one sale of one share of Apple stock yesterday.
+    Input: [10, 7, 5, 8, 11, 9]
+    Output: 6
+'''
+def get_max_profit(stock_prices):
+    
+    if len(stock_prices) < 2:
+        raise ValueError('max profit need at least 2 prices')
+        
+    curr_min = stock_prices[0]
+    max_profit = stock_prices[1] - stock_prices[0]
+    
+    for curr_price in stock_prices[1:]:
+        pot_profit = curr_price - curr_min
+        max_profit = max(max_profit, pot_profit)
+        curr_min = min(curr_min, curr_price)
+
+    return max_profit

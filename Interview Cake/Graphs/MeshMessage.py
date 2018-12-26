47a24da39fb4637199995a9355e7409491cd004c
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-26 15:44:19 -0500

    Create MeshMessage.py

---
+++ Interview Cake/Graphs/MeshMessage.py
@@ -0,0 +1,34 @@
+'''
+Mesh Message:
+    Find the shortest route for a message from one user (the sender) to another (the recipient). 
+    Return a list of users that make up this route.
+'''
+def bfs_get_path(graph, start, end):
+
+    if start not in graph or end not in graph: # error if the nodes are not in the graph
+        raise Exception("start or end not in graph")
+
+    visited = set() # a set of visited nodes
+    parents = {} # a map that contains that shortest path from the child to the parent
+    
+    queue = [] # a queue to bfs
+    queue.append(start)
+    
+    while queue:
+        node = queue.pop(0)
+        for neighbhor in graph[node]:
+            if neighbhor not in visited:
+                queue.append(neighbhor) # add all the neighbhors to the queue
+                visited.add(neighbhor) # add the neighbhors to the visited set
+                parents[neighbhor] = node # set the child : parent binding
+    path = []
+    curr = end
+    while curr is not start: # build the path until the start value
+        path.insert(0, curr)
+        if curr not in parents: # 
+            return None
+        curr = parents[curr]
+    
+    path.insert(0, start)
+
+    return path

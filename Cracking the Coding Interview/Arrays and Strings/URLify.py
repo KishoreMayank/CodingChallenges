f85e1f33c6caef499ab12389f2cc32797a1a4d02
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-27 22:48:30 -0500

    Create URLify.py

---
+++ Cracking the Coding Interview/Arrays and Strings/URLify.py
@@ -0,0 +1,19 @@
+'''
+URLify:
+    Write a method to replace all the spaces in a string with '%20'. The string has sufficient space to hold it
+    Input:  "Mr John Smith    "
+    Output: "Mr%20John%20Smith"
+'''
+
+def urlify(string, length):
+    new_index = len(string) # start from the back of the array
+
+    for i in reversed(range(length)): # reverses the range
+        if string[i] == ' ':
+            string[new_index - 3:new_index] = '%20' # when you find a space, replace it with chars
+            new_index -= 3
+        else:
+            string[new_index - 1] = string[i] # otherwise, keep moving the characters one towards the back of the array
+            new_index -= 1
+
+    return string

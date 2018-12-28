aa30c92fffc5fee42e8853dc0807e27711c3136a
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-27 22:31:41 -0500

    unbounded knapsack

---
+++ Interview Cake/Recursion and Dynamic Programming/UnboundedKnapsack.py
@@ -0,0 +1,18 @@
+'''
+Unbounded Knapsack:
+    Given a knapsack with a capacity and cakes with a (weight, value), calculate the maximum value 
+'''
+
+def unbounded_knapsack(cake_tuples, capacity):
+    
+    combos = [0] * (capacity + 1)
+
+    for cake in cake_tuples:
+        weight = cake[0]
+        value = cake[1]
+        if weight == 0 and value > 0:
+            return float('inf')
+        for i in range(weight, capacity + 1): # for every cake in the range [weight, capacity + 1)
+            # take the max of the current or the value at (i - weight) + value
+            combos[i] = max(combos[i], combos[i - weight] + value) 
+    return combos[capacity]

2a4f242e7d0046cd6e99121bcc6f73d219c8c502
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-27 23:02:37 -0500

    Update UnboundedKnapsack.py

--- Interview Cake/Recursion and Dynamic Programming/UnboundedKnapsack.py
+++ Interview Cake/Recursion and Dynamic Programming/UnboundedKnapsack.py
@@ -7,9 +7,7 @@ def unbounded_knapsack(cake_tuples, capacity):
     
     combos = [0] * (capacity + 1)
 
-    for cake in cake_tuples:
-        weight = cake[0]
-        value = cake[1]
+    for weight, value in cake_tuples:
         if weight == 0 and value > 0:
             return float('inf')
         for i in range(weight, capacity + 1): # for every cake in the range [weight, capacity + 1)

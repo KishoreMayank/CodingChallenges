52edcd82f9d83ec45a6ea2078bf0b04d2fdafdd7
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-28 11:33:18 -0500

    tester

--- Cracking the Coding Interview/Arrays and Strings/URLify.py
+++ Cracking the Coding Interview/Arrays and Strings/URLify.py
@@ -5,15 +5,19 @@ URLify:
     Output: "Mr%20John%20Smith"
 '''
 
-def urlify(string, length):
+def urlify(string, length): # takes in a string as a list
     new_index = len(string) # start from the back of the array
 
     for i in reversed(range(length)): # reverses the range
         if string[i] == ' ':
-            string[new_index - 3:new_index] = '%20' # when you find a space, replace it with chars
+            string[new_index - 3 : new_index] = '%20' # when you find a space, replace it with chars
             new_index -= 3
         else:
             string[new_index - 1] = string[i] # otherwise, keep moving the characters one towards the back of the array
             new_index -= 1
 
     return string
+
+def main():
+    urlify(list('Mr John Smith    '), 13)
+main()

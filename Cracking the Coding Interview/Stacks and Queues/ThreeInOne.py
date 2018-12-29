3ef2fccefdc2f7f386af679088c07b0f552e3f34
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-28 23:08:51 -0500

    Create ThreeInOne.py

---
+++ Cracking the Coding Interview/Stacks and Queues/ThreeInOne.py
@@ -0,0 +1,41 @@
+'''
+Three In One:
+    Use a single array to implement three stacks
+'''
+
+class MultiStack:
+    
+    def __init__(self, stacksize):
+        self.numstacks = 3
+        self.array = [0] * (stacksize * self.numstacks)
+        self.sizes = [0] * self.numstacks
+        self.stacksize = stacksize
+
+    def Push(self, item, stacknum): # push into the desired stack
+        if self.IsFull(stacknum):
+            raise Exception('Stack is full')
+        self.sizes[stacknum] += 1
+        self.array[self.IndexOfTop(stacknum)] = item
+
+    def Pop(self, stacknum): # pop out of the desired stack
+        if self.IsEmpty(stacknum):
+            raise Exception('Stack is empty')
+        value = self.array[self.IndexOfTop(stacknum)]
+        self.array[self.IndexOfTop(stacknum)] = 0
+        self.sizes[stacknum] -= 1
+        return value
+
+    def Peek(self, stacknum): # peek the desired stack
+        if self.IsEmpty(stacknum):
+            raise Exception('Stack is empty')
+        return self.array[self.IndexOfTop(stacknum)]
+
+    def IsEmpty(self, stacknum):
+        return self.sizes[stacknum] == 0
+
+    def IsFull(self, stacknum):
+        return self.sizes[stacknum] == self.stacksize
+
+    def IndexOfTop(self, stacknum):
+        offset = stacknum * self.stacksize
+        return offset + self.sizes[stacknum] - 1

f2a4966ebe373f1a3c9acbb2c9b16392017eb79c
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-29 17:47:00 -0500

    Create Successor.py

---
+++ Cracking the Coding Interview/Trees and Graphs/Successor.py
@@ -0,0 +1,17 @@
+'''
+Successor:
+    Find the next node of a given node in a BST
+'''
+
+def inOrderSuccessor(n):
+    if n.right:
+        n = n.right # if there is a right subtree, find the value by traversing down it
+        while n.left:
+            n = n.left
+        return n
+
+    p = n.parent 
+    while p and n != p.right: # if it's on the left, keep going up till the child is the left of the parent
+        n = p  
+        p = p.parent 
+    return p 

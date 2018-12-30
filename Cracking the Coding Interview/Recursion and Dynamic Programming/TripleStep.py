a7191f9dbea9a12ca76a6daac455542c164b735e
Mayank Kishore <mayankkishore.in@gmail.com>
2018-12-30 18:55:27 -0500

    Create TripleStep.py

---
+++ Cracking the Coding Interview/Recursion and Dynamic Programming/TripleStep.py
@@ -0,0 +1,19 @@
+'''
+Triple Step:
+    A child can run up and hop 1, 2, or 3 steps at a time. How many ways can it make it up the stairs
+'''
+
+# DYNAMIC PROGRAMMING (TOP - DOWN)
+memo = {0:1, 1:1, 2:2}
+def triple_step(n):
+
+    if n in memo:
+        return memo[n]
+    memo[n] = triple_step(n - 1) + triple_step(n - 2) + triple_step(n - 3) # store the value of the step of the current
+        
+    return memo[n]
+
+def main():
+    print(triple_step(5))
+
+main()
